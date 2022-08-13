e356a8b29d8c5cd48b0f803ec9c88e1c9e5a6f25
adamrayw <raywibowo68@gmail.com>
2022-08-13 14:29:45 +0700

    add port

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 
-const port = process.env.PORT || 8080
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', async (req, res) => {
-
-    const created = await prisma.user.create({
-        data: {
-            name: 'Adam',
-            email: 'raywibowo68@gmail.com',
-            password: '12345678'
-        }
-    })
-
-    res.json({
-        'message': created
-    })
-})
-
 app.use('/api/', userRoutes)
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log(`Server running on http://localhost:${port}`)
 })
