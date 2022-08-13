a1597f7159067cb0e80dabb51eeab90151088912
adamrayw <raywibowo68@gmail.com>
2022-08-13 15:04:09 +0700

    fix fix

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 
 const app = express()
 
+var allowedOrigins = ['https://usedup.vercel.app/'];
+
+app.use(cors({
+
+    origin: function (origin, callback) {
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+
+    exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+
+    credentials: true,
+}));
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://usedup.herokuapp.com/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.get('/', (req, res) => {
     res.json({
