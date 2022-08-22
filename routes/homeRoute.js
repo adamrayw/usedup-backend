060ab8a7e485407276e1c966cd7b7fda86db4dcd
adamrayw <raywibowo68@gmail.com>
2022-08-22 14:04:53 +0700

    add query result

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { home, view, kategori, kategoriItems } = require('../contollers/homeControllers');
 const router = express.Router()
 
-router.get('/home', home)
+router.get('/home?:result', home)
 router.get('/kategori', kategori)
 router.get('/:id', view)
 router.get('/kategori/:slug', kategoriItems)
