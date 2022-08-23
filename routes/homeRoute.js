d093d1d164e44a244acaccbcbcc48a30323c60b4
adamrayw <raywibowo68@gmail.com>
2022-08-23 11:47:22 +0700

    add delete route

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { home, view, kategori, kategoriItems } = require('../contollers/homeControllers');
+const { home, view, kategori, kategoriItems, deleteData } = require('../contollers/homeControllers');
 const router = express.Router()
 
 router.get('/home?:result', home)
 router.get('/kategori', kategori)
 router.get('/:id', view)
 router.get('/kategori/:slug', kategoriItems)
+router.delete('/delete', deleteData)
 
 module.exports = router
