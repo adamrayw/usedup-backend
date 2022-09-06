86ba868f366bdb26ed6593aa2afd2bbedc5e720c
adamrayw <raywibowo68@gmail.com>
2022-09-07 02:54:15 +0700

    forgot password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { userRegister, userLogin, updateUser, verification, sendEmailVerification } = require('../controllers/userControllers');
+const { userRegister, userLogin, updateUser, verification, sendEmailVerification, forgotPassword, resetPassword, resetPasswordPost } = require('../controllers/userControllers');
 
 const router = express.Router()
 
@@ -8,5 +8,8 @@ router.post('/login', userLogin)
 router.post('/update', updateUser)
 router.post('/verification?:id', verification)
 router.post('/sendemailverif', sendEmailVerification)
+router.post('/forgotpassword', forgotPassword)
+router.post('/resetpassword', resetPassword)
+router.post('/resetpasswordpost', resetPasswordPost)
 
 module.exports = router
