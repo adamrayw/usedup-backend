63c64ebe586fe201974af9bbd56a9e00df5944da
adamrayw <raywibowo68@gmail.com>
2022-09-20 21:58:21 +0700

    protect update profile API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { userRegister, userLogin, updateUser, verification, sendEmailVerification, forgotPassword, resetPassword, resetPasswordPost } = require('../controllers/userControllers');
-
+const { validateToken } = require("../middleware/validateToken");
 const router = express.Router()
 
 router.post('/register', userRegister)
 router.post('/login', userLogin)
-router.post('/update', updateUser)
+router.post('/update', validateToken, updateUser)
 router.post('/verification?:id', verification)
 router.post('/sendemailverif', sendEmailVerification)
 router.post('/forgotpassword', forgotPassword)
