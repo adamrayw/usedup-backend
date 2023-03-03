5e06dd1a243b0f24e553d27b57f6969cccfeca29
adamrayw <raywibowo68@gmail.com>
2023-03-03 15:08:41 +0700

    add origin in cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const http = require('http');
 const app = express()
 const server = http.createServer(app)
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 // Socket.io
 const io = new Server(server, {
@@ -20,13 +20,29 @@ const io = new Server(server, {
     }
 })
 
+// create a new notification
+
+// create a new notification
+
+// emit a notification event to all connected clients
+
 io.on("connection", (socket) => {
     console.log(socket.id);
+
+    const notification = {
+        id: 1,
+        message: 'New notification',
+    };
+
+    socket.on('notification', () => {
+        console.log('notif')
+    });
 
     socket.on("join_room", (data) => {
         socket.join(data.room)
     })
 
+    /* Listening for a message from the client and then sending it to the room. */
     socket.on("send_message", (data) => {
         console.log(data);
         socket.to(data.room).emit("receive_message", data)
@@ -48,6 +64,9 @@ app.use('/api/', homeRoute)
 app.use('/api/', chatRoutes)
 
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log('Server running...');
+// server.listen(process.env.PORT || 3000, () => {
+//     console.log('Server running...');
+// })
+server.listen(3001, () => {
+    console.log('Server running...' + process.env.PORT);
 })
